52186204a268fe6ee42307c77a5b485777e376f0
pushkarchauhan91 <pushkarchauhan91@pushkarchauhan91s-MacBook-Air.local>
2024-05-13 19:26:18 +0530

    init commit

---
+++ src/test/java/com/leetcode/LeetcodeSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
